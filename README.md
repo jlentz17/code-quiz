ee8884ec1c5defa7c066a2be731ce7b020b3d66a
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-05-28 11:53:28 -0700

    updated readme and questions

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# code-quiz
+# Code Quiz
+
+[For deployed version, click me!!!](https://jlentz17.github.io/code-quiz/)
+
+In the fourth week of bootcamp, I was given the assignment of making a code quiz with JavaScript and WEb API's.
+
+
+![Image of Code Quiz](./codeQuizScreenshot.png/)
+
+# Description 
+
+## User Story
+
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers.
+
+## Acceptance Criteria
+
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+
+## Built With
+
+* [Visual Studio Code](https://code.visualstudio.com/)
+* [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+* [CSS 3](https://developer.mozilla.org/en-US/docs/Web/CSS)
+* [HTML 5](https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5)
+* [Markdown](https://markdownguide.org/cheat-sheet/)
+
+
+## Contributors
+
+Jeremy Lentz <br> <jlentz17@gmail.com> <br> &copy; 2020 Jeremy Lentz. All rights reserved.
--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var questionText = document.querySelector("#questionText");
 var beginQuiz = document.querySelector("#beginQuiz");
 var possibleAnswers = document.querySelector("#possibleAnswers");
 var viewScore = document.querySelector("#score");
-var viewHighScores = document.querySelector(".viewHighScores")
+var viewHighScores = document.querySelector(".viewHighScores");
 var showScore = document.querySelector(".showScore");
 var hideHighScores = document.querySelector(".hideHighScores");
 
@@ -16,84 +16,88 @@ var score = 0;
 var timeLeft = 100;
 var timerInterval;
 var questions = [
+  {
+    questionTitle: "What year is it???",
+    questionChoices: ["1927", "2055", "It was brand new!", "orange"],
+    questionAnswer: "It was brand new!",
+  },
 
-    {questionTitle: "What year is it???", 
-    questionChoices: ["1927", "2055", "It was brand new!", "orange"], 
-    questionAnswer: "It was brand new!"},
+  {
+    questionTitle: "Is Javascript fun?",
+    questionChoices: ["yes", "green", "maybe", "eh"],
+    questionAnswer: "eh",
+  },
 
-    {questionTitle: "Is Javascript fun?", 
-    questionChoices: ["yes", "green", "maybe", "eh"], 
-    questionAnswer: "eh"},
+  {
+    questionTitle: "How does a For loop start?",
+    questionChoices: ["for(i=0, i<question.length, i--)", "for(i====0)", "looooop", "for(i = 0; i<questions.length; i++)"],
+    questionAnswer: "for(i = 0; i<questions.length; i++)"
+  },       
+ 
+  {
+    questionTitle: "Which of the following type of variable is visible only within a function where it is defined?",
+    questionChoices: [["roy", "gee", "biv"], "global variable", "local variable", "var"],
+    questionAnswer: "local variable"
+  },
 
-    {questionTitle: "????", 
-    questionChoices: ["blue", "green", "purple", "orange"], 
-    questionAnswer: "blue"},
-
-    {questionTitle: "What color is t?", 
-    questionChoices: ["blue", "green", "purple", "orange"], 
-    questionAnswer: "green"},
-
-    {questionTitle: "What color is the sky?", 
-    questionChoices: ["blue", "green", "purple", "orange"], 
-    questionAnswer: "purple"}
+  {
+    questionTitle: "JavaScript is a ___ -side programming language.",
+    questionChoices: ["none", "client", "both", "server"],
+    questionAnswer: "both",
+  }
 ];
 
 startButton.addEventListener("click", startQuiz);
 
-function startQuiz(){
+function startQuiz() {
   // make beginQuiz invisible
   beginQuiz.classList.add("hide");
-    // when start quiz call timer start function
-    // make beginQuiz invisible
+  // when start quiz call timer start function
+  // make beginQuiz invisible
   questionContainer.classList.remove("hide");
-    // call displayQuestion function here
-nextQuestion();
-startTimer();
+  // call displayQuestion function here
+  nextQuestion();
+  startTimer();
 }
 
-function nextQuestion(){
-  if (index === questions.length){
+function nextQuestion() {
+  if (index === questions.length) {
     return endQuiz();
   }
-//   var currentQuestion = questions[index];
+  //   var currentQuestion = questions[index];
   possibleAnswers.innerHTML = "";
   questionText.innerText = questions[index].questionTitle;
   questions[index].questionChoices.forEach(function (item) {
     var btn = document.createElement("button");
     btn.textContent = item;
     possibleAnswers.append(btn);
-})
+  });
 }
 
-possibleAnswers.addEventListener("click", function(event){
-    // if answer is wrong, exit
-    console.log(event.target.textContent);
-    if (event.target.textContent === questions[index].questionAnswer){
-        score++;
-        viewScore.textContent = score;
+possibleAnswers.addEventListener("click", function (event) {
+  // if answer is wrong, exit
+  console.log(event.target.textContent);
+  if (event.target.textContent === questions[index].questionAnswer) {
+    score++;
+    viewScore.textContent = score;
 
+    // figure out how to print score with DOM manip. make new div id= score, then queryselect!
 
-        // figure out how to print score with DOM manip. make new div id= score, then queryselect!
+    index++;
+    console.log(index);
 
-        index++;
-        console.log(index);
-
-        nextQuestion();
-    } else {
-      if(
-        timeLeft < 0
-      ){
-        alert("game over");
-        endQuiz();
-      }
-        timeLeft -= 10;
-        console.log(timeLeft);
-        viewScore.textContent = score;
+    nextQuestion();
+  } else {
+    if (timeLeft < 0) {
+      alert("GAME OVER");
+      endQuiz();
     }
-}
-);
+    timeLeft -= 10;
+    console.log(timeLeft);
+    viewScore.textContent = score;
+  }
+});
 var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
 
 function endQuiz() {
   // make a prompt that asks for init.
@@ -113,33 +117,31 @@ var playerInfo = [];
 // hide gameboard
 
 function startTimer() {
-    timerInterval = setInterval(function () {
-      timeLeft--;
-      timerEl.textContent = timeLeft;
-      if (timeLeft <= 0) {
-        // End the game
-        // Stop the timer
-        clearInterval(timerInterval);
-      }
-      // when timer hits 0, end game
-    }, 1000);
-    return timeLeft;
-  }
-  // remove and hide each button
-    viewHighScores.addEventListener("click", function(){
-      var Initials = localStorage.getItem("Initials");
-      var playerScore = localStorage.getItem("playerScore");
-      showScore.classList.remove("hide");
-      showScore.innerHTML = `${Initials} has a high score of ${playerScore}`;
+  timerInterval = setInterval(function () {
+    timeLeft--;
+    timerEl.textContent = timeLeft;
+    if (timeLeft <= 0) {
+      // End the game
+      // Stop the timer
+      clearInterval(timerInterval);
+    }
+    // when timer hits 0, end game
+  }, 1000);
+  return timeLeft;
+}
+// remove and hide each button
+viewHighScores.addEventListener("click", function () {
+  var Initials = localStorage.getItem("Initials");
+  var playerScore = localStorage.getItem("playerScore");
+  showScore.classList.remove("hide");
+  showScore.innerHTML = `${Initials} has a high score of ${playerScore}`;
+  
+  viewHighScores.classList.add("hide");
+  hideHighScores.classList.remove("hide");
+});
 
-      viewHighScores.classList.add("hide");
-      hideHighScores.classList.remove("hide");
-    });
-
-    hideHighScores.addEventListener("click", function(){
-      hideHighScores.classList.add("hide");
-      showScore.classList.add("hide");
-      viewHighScores.classList.remove("hide");
-      
-    });
-
+hideHighScores.addEventListener("click", function () {
+  hideHighScores.classList.add("hide");
+  showScore.classList.add("hide");
+  viewHighScores.classList.remove("hide");
+});
